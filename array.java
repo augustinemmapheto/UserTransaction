d3b16b117b0376afee3e913382b68a9d071fd4ab
augustine_dev <augustinemmapheto@gmail.com>
2024-05-31 21:04:52 +0200

    with search and sort

--- array.java
+++ array.java
@@ -15,10 +15,30 @@ public class Overnight
         int[] ages = {19, 30, 21, 20, 17};
         int index = -1;
         
-        /*for(int i = 0; i < name.length; i++)
+        int temp = 0;
+        String temp1 = "";
+        for(int i = 0; i < names.length -1; i++)
         {
-            
-        }*/
+            for(int j = 0; j < ages.length -1; j++)
+            {
+                //less< represent descending whereas great > is assending
+                if(ages[j] > ages[j + 1])
+                {
+                    temp = ages[j];
+                    ages[j] = ages[j + 1];
+                    ages[j + 1] = temp;
+                    
+                    temp1 = names[j];
+                    names[j] = names[j + 1];
+                    names[j + 1] = temp1;
+                }
+            }
+        }
+        
+        for(int i = 0; i < ages.length; i++)
+        {
+           System.out.println( names[i] + "\t " + ages[i]);
+        }
         
         for(int i = 0; i < names.length; i++)
         {
@@ -36,6 +56,7 @@ public class Overnight
             System.out.println( sName + " doesn't exist " ); 
         }
         
+        
         //String xtract = sName.substring(sName.indexOf(" "));
         // extract the word java
         
@@ -46,8 +67,16 @@ public class Overnight
         String x = sWord.substring(sWord.indexOf("J") , sWord.lastIndexOf(" "));
         
          System.out.println( x);*/
+        
+        
+       
         //System.out.println(xtract);
-
+        
+       
+        
+        
+        
+        
     }
     
 }
